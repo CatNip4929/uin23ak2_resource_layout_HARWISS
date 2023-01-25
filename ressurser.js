cfb3d6cc07473eb9922e89c7023cb402a8301790
CatNip4929 <111730636+CatNip4929@users.noreply.github.com>
2023-01-25 19:57:35 +0100

    endring i oppsett både i html, css og javascript
    siste endring før innlevering, fikset, ny mapping av resource henter ut bare tittel, putter inn å bytter ut med button textene, avhengig av id. fjernet figure og divs for å kunne få gjort on button knappene til en array for å endre på classenavne til knappen(endre på hvilken css classe jeg skal bruke avhengig av hvilken som er trykket.)
    
    fjernet gjennbruk av id'er bruker klasser i stedet.
    
    Css rettet noen classenavn og id'r for å unngå å forvirring i hvilke id'r og classer jeg skulle ha tak i. endret størrelsen på teksten, endret font-size, endret font-family, likt men forksjellig farge i .button og .aktive .button:hover.
    
    javascript for å bytte mellom forskjellige klasser, ved å bruke for loop av div arrayen. lagt til sudokode med forklaring av koden, hva den gjør og hvordan den fungerer til mitt nåværende beste evne og forståelse.

--- ressurser.js
+++ ressurser.js
@@ -94,3 +94,118 @@ const resources = [
         ]
     },
 ]
+
+
+// lag variabel for å mappe resources, lag et parameter navn : "cat"/category.
+// ta html teksten for ordnet liste å sett inn i en variabel
+// lag variabel for å lagre template litaral strengene til h2 taggen. I mellom h2 taggene reffer til variabelen i resources med navnet category
+var oneList = resources.map(cat => { 
+    let olList = "<ol>"
+    let firstPart = `
+     <h2>${cat.category}</h2>
+     <p> ${cat.text}</p>`
+// I arrayen resources har vi enda en array, multidimensjonal array med navn sources. bruker samme metode fortsette mappingen fra resources til sources, (her var resources og sources nå brukt, valgte å kalle den sauce :| )
+     cat.sources.map(sauce => olList += `<li><a href="${sauce.url}">${sauce.title} </a></li>` )
+     //konkantiner olList med ordnet liste end tag.
+     olList += "</ol>"
+     //ta den første delen av mappingen og legg til den nå ordnede listen med linker.
+    firstPart += olList
+    return firstPart
+     }
+)
+
+var tittelRender = resources.map(cat => {
+    let ting = `${cat.category}`
+    return ting
+})
+
+document.getElementById("HTML").innerHTML = tittelRender[0]
+document.getElementById("CSS").innerHTML = tittelRender[1]
+document.getElementById("javaScript").innerHTML = tittelRender[2]
+document.getElementById("react").innerHTML = tittelRender[3]
+document.getElementById("sanityCms").innerHTML = tittelRender[4]
+
+
+// console.log(oneList[1])
+
+
+
+       
+//  https://www.w3schools.com/howto/howto_js_active_element.asp
+// finn div elementene i html som har id kate, lagre dette i en variabel for å så å finne elementer med classe navn button.
+        var div = document.getElementById("kate")
+        var button = div.getElementsByClassName("button")
+        console.log(button.length)
+
+        // forloop som sjekker alle elementene i div med classe button, for hvert av elementene lagre "aktive elementer i current".
+        // var button er nå en liste med classer. med en addEventListener kan jeg sjekke om elementet blir klikket på, dermed ønsker jeg å legge til-
+        //texten " active" for å endre classe navnet "button" i HTML til "button active". dermed kan jeg lage css kode som er klar til bruk i tifelle den skulle endre navn med gitte egenskaper.
+
+        for (var i = 0; i < button.length; i++) {
+            button[i].addEventListener("click", function() {
+                // console.log("test")
+                var current = document.getElementsByClassName("active")
+                current[0].className = current[0].className.replace(" active", "")
+                this.className += " active"
+        });
+    }
+ 
+    
+
+// let categoryHTML = ""
+
+// funksjoner for å hente ønsket del av listen, hvis listen er åpen, mulighet for å navigere til neste kategori.
+// Å gjøre, hvis listen er åpen "aktiv" endre denne knappens bakgrunnsfarge til til turquoise, og color til lightwhite.
+// Å gjøre, hvis denne knappen er åpen skal den kunne lukkes, hvis onclicken ikke er denne knappen, åpne.
+// kan vurdere å lage en class til buttons, og id til hver enkelt, da kan jeg sette fargen slik jeg vil avhengig av hvilken jeg trykker på.
+
+document.querySelector("#articles").classList.toggle("hidden")
+
+document.querySelector("#articles").innerHTML = oneList[0]
+// Classlist.toggle velger css classen hidden, siden jeg har laget en css classe som heter hidden velger den stilen til classen i css filen.
+//hvis jeg lager en class som heter feks visible button eller aktive kan jeg kansje få til å toggle de forskjellige buttonsene avhengig om jeg har trykket på dem.
+// kan kansje vurdere å lage, finner en funksjon som sjekker om elementet er synlig.
+function toggleCategoryZero() {
+    
+    if (document.querySelector("#articles").classList.toggle("hidden")) {
+        document.querySelector("#articles").innerHTML = oneList[0]
+        document.querySelector("#articles").classList.toggle("hidden")
+        
+    } else { 
+        document.querySelector("#articles").innerHTML = oneList[0]
+            
+        
+    }
+}
+function toggleCategoryOne() {
+    if (document.querySelector("#articles").classList.toggle("hidden")){
+        document.querySelector("#articles").innerHTML = oneList[1]
+        document.querySelector("#articles").classList.toggle("hidden")
+    } else {
+        document.querySelector("#articles").innerHTML = oneList[1]
+    }   
+}
+function toggleCategoryTwo() {
+    if (document.querySelector("#articles").classList.toggle("hidden")){
+        document.querySelector("#articles").innerHTML = oneList[2]
+        document.querySelector("#articles").classList.toggle("hidden")
+    } else {
+        document.querySelector("#articles").innerHTML = oneList[2]
+    } 
+}
+function toggleCategoryThree() {
+    if (document.querySelector("#articles").classList.toggle("hidden")){
+        document.querySelector("#articles").innerHTML = oneList[3]
+        document.querySelector("#articles").classList.toggle("hidden")
+    } else {
+        document.querySelector("#articles").innerHTML = oneList[3]
+    }
+}
+function toggleCategoryFour() {
+    if (document.querySelector("#articles").classList.toggle("hidden")){
+        document.querySelector("#articles").innerHTML = oneList[4]
+        document.querySelector("#articles").classList.toggle("hidden")
+    } else {
+    document.querySelector("#articles").innerHTML = oneList[4]
+    }
+}
